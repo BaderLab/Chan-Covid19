6b2502af58571d19fd07e08e3ae993fc7b2baf07
Shraddha Pai <shraddha.pai@utoronto.ca>
2020-04-06 23:38:17 +0000

    runs end to end

--- genome_clean/findOutliers.R
+++ genome_clean/findOutliers.R
@@ -1,8 +1,12 @@
 # find seqs with outlier num gaps
-
 args <- commandArgs(TRUE)
 inFile <- args[1]
 
+dt <- format(Sys.Date(),"%y%m%d")
+logFile <- sprintf("%s_%s.log",inFile,dt)
+
+sink(logFile,split=TRUE)
+tryCatch({
 dat <- read.delim(inFile,sep="\t",h=F,as.is=T)
 colnames(dat) <- c("seqname","seqlen","numA","numC","numG","numT",
 	"numN","numGaps")
@@ -16,37 +20,64 @@ dat$virus <- unlist(lapply(nm, function(x) x[1]))
 dat$country <- ctry
 dat$region <- ser2
 
-pdf(sprintf("%s_numNandGaps.pdf",inFile))
-message("Num N")
-hist(dat$numN,n=100,main="num N")
-qtl <- quantile(dat$numN, c(0.25,0.5,0.75,0.8,0.9,0.95,0.99))
-abline(v=qtl,lty=3,col='red')
-print(qtl)
+outDir <- dirname(inFile)
+baseF <- basename(inFile)
 
-nthresh <- qtl[6]
-idx <- which(dat$numN > nthresh) # top 5% worst
-write.table(dat[idx,],
-	file=sprintf("%s_top5pct_numN.txt",inFile),
-	sep="\t",col=T,row=F,quote=F)
+pdf(sprintf("%s_numNandGaps_%s.pdf",inFile,dt))
+tryCatch({
+	cat("Num N\n")
+	hist(dat$numN,n=100,main="num N")
+	qtl <- quantile(dat$numN, c(0.25,0.5,0.75,0.8,0.9,0.95,0.99))
+	abline(v=qtl,lty=3,col='red')
+	print(qtl)
 
-message("Gaps")
-hist(dat$numGaps,n=100,main="num Gaps")
-qtl <- quantile(dat$numGaps, 
-	c(0.25,0.5,0.75,0.8,0.9,0.95,0.99,0.995,0.999,0.9999))
-print(qtl)
-abline(v=qtl,lty=3,col='red')
+	nthresh <- 550
+	idx <- which(dat$numN > nthresh) # top 5% worst
+	write.table(dat[idx,],
+		file=sprintf("%s_top5pct_numN_%s.txt",inFile,dt),
+		sep="\t",col=T,row=F,quote=F)
+	# violator 1 - too many N's
+	write.table(dat[idx,1],
+		file=sprintf("%s/fail_%s_NViolate_IDs_%s.txt",outDir,baseF,dt),
+		sep="\t",col=F,row=F,quote=F)
 
-plot(dat$numGaps,dat$numN, xlab="num gaps",ylab="num N")
-abline(0,1,col='red')
-abline(h=nthresh,v=10,lty=3,col='red')
+	cat("Gaps\n")
+	hist(dat$numGaps,n=100,main="num Gaps")
+	qtl <- quantile(dat$numGaps, 
+		c(0.25,0.5,0.75,0.8,0.9,0.95,0.99,0.995,0.999,0.9999))
+	print(qtl)
+	abline(v=qtl,lty=3,col='red')
+	
+	plot(dat$numGaps,dat$numN, xlab="num gaps",ylab="num N")
+	abline(0,1,col='red')
+	abline(h=nthresh,v=10,lty=3,col='red')
 
-idx <- which(dat$numGaps > 10) # top 5% worst
-write.table(dat[idx,],
-	file=sprintf("%s_GapViolate.txt",inFile),
-	sep="\t",col=T,row=F,quote=F)
-write.table(dat[idx,1],
-	file=sprintf("%s_GapViolate_IDs.txt",inFile),
-	sep="\t",col=T,row=F,quote=F)
+	# violator 2 - too many gaps
+	idx <- which(dat$numGaps > 10) # top 5% worst
+	write.table(dat[idx,],
+		file=sprintf("%s_GapViolate_%s.txt",inFile,dt),
+		sep="\t",col=T,row=F,quote=F)
+	write.table(dat[idx,1],
+		file=sprintf("%s/fail_%s_GapViolate_IDs_%s.txt",outDir,baseF,dt),
+		sep="\t",col=F,row=F,quote=F)
 
-dev.off()
+	# violator 3 - seq too long
+	idx <- which(dat$seqlen > 30000)
+	if (any(idx)) {
+		cat(sprintf("%i with seqs too long!", length(idx)))
+		write.table(dat[idx,1],
+			file=sprintf("%s/fail_%s_LenViolate_IDs_%s.txt",outDir,baseF,dt),
+				sep="\t",col=F,row=F,quote=F)
+	}
 
+},error=function(ex){
+	print(ex)
+},finally={
+	dev.off()
+})
+
+},error=function(ex){
+	print(ex)
+},finally={
+	sink(NULL)
+})
--- genome_clean/qcGenomes.sh
+++ genome_clean/qcGenomes.sh
@@ -2,14 +2,37 @@
 
 genomeFile=/home/project_resources/data/genomes/gisaid_cov2020_sequences_hCoV19_HumanHost_Complete_LowCovExcl_200403.fasta
 
+outDir=~/genomeQC
+
+
+dt=`date '+%y%m%d'` 
+mkdir $dt
+outDir=${outDir}/${dt}
+
 # count N and gaps
 baseF=`basename $genomeFile .fasta`
 echo "counting gaps"
-perl countN.pl < $genomeFile > ${baseF}_countN.txt
+perl countN.pl < $genomeFile > ${outDir}/${baseF}_countN_${dt}.txt
 
 echo "finding outliers"
-Rscript findOutliers.R ${baseF}_countN.txt
+Rscript findOutliers.R ${outDir}/${baseF}_countN_${dt}.txt
 
 # create new fasta file with outliers removed
 echo "removing outliers"
-$ awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' $genomeFile | grep -Ff ${baseF}_countN.txt_GapViolate_IDs.txt - | tr "\t" "\n" > out.fa
+ awk '{ 
+	if ((NR>1)&&($0~/^>/)) { 
+		printf("\n%s", $0); 
+	} else if (NR==1) { 
+		printf("%s", $0); 
+	} else { 
+		printf("\t%s", $0); 
+	} 
+}' $genomeFile > tmp.txt
+
+# keep sequences with headers that don't match those in the file
+cat ${outDir}/fail*.txt  | sort -k1,1 | uniq > ${outDir}/failIDs.txt
+numFail=`wc -l ${outDir}/failIDs.txt`
+echo "$numFail genomes fail tests."
+grep -vFf ${outDir}/failIDs.txt tmp.txt |  tr "\t" "\n" > ${outDir}/${baseF}_passQC.fa
+
+rm tmp.txt
