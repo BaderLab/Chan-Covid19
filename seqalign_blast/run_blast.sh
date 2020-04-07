9fb2873e5a852f3249ec16e83ece120e2db85dca
Shirley Hui <shirley.hui@utoronto.ca>
2020-04-07 04:52:42 +0000

    Script now sorts and retains only top hits per probe and viral genome

--- seqalign_blast/run_blast.sh
+++ seqalign_blast/run_blast.sh
@@ -1,13 +1,19 @@
-PROBE_NAME=$1
-PROBE_SEQ=$2
-TYPE=$3
+#PROBE_NAME=$1
+#PROBE_SEQ=$2
+#TYPE=$3
 
-echo $PROBE_SEQ $PROBE_NAME
+#echo $PROBE_SEQ $PROBE_NAME
 
-printf ">$PROBE_NAME\n$PROBE_SEQ" > probe_${PROBE_NAME}_${TYPE}.fa
+#printf ">$PROBE_NAME\n$PROBE_SEQ" > probe_${PROBE_NAME}_${TYPE}.fa
+
+PROBE_FA_FILENAME=$1
+SUBJECT_FA_FILENAME=$2
+
+echo "Probe fasta file: ${PROBE_FA_FILENAME}"
+echo "Subject fasta file: ${SUBJECT_FA_FILENAME}"
 
 sudo docker run --rm \
 	-v /home/project_resources/data/genomes:/blast/blast_covid/genomes:ro \
 	-v /home/shui/software/Chan-Covid19/seqalign_blast:/blast/blast_covid/out:rw \
 	-w /blast/blast_covid/out \
-	ncbi/blast blastn -task blastn-short -num_alignments 535 -query /blast/blast_covid/out/probe_${PROBE_NAME}_${TYPE}.fa -subject /blast/blast_covid/genomes/SARS-CoV2-GISAID_USA_isolates_200327.fa -outfmt "6" -out /blast/blast_covid/out/blast_out_${PROBE_NAME}_${TYPE}.txt
+	ncbi/blast blastn -task blastn-short -num_alignments 3000 -query /blast/blast_covid/out/${PROBE_FA_FILENAME} -subject /blast/blast_covid/out/${SUBJECT_FA_FILENAME} -outfmt 6 | sort -k2,2 -k12,12nr -k11,11n | sort -u -k2,2 --merge > /home/shui/software/Chan-Covid19/seqalign_blast/blast_out_${PROBE_FA_FILENAME%.fa}_all_tophits.txt
